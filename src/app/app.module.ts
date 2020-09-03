f8807e607fb667103c238482038d31337298fd36
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-09-03 19:11:51 -0400

    ng-switch-example and added semantic-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgSwitchExampleComponent } from './ng-switch-example/ng-switch-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgSwitchExampleComponent
   ],
   imports: [
     BrowserModule,
