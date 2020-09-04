2536ccab9e6059999043f8a57e012a4f886fb558
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-09-04 17:05:56 -0400

    NgClass example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgSwitchExampleComponent } from './ng-switch-example/ng-switch-example.component';
 import { NgStyleExampleComponent } from './ng-style-example/ng-style-example.component';
+import { NgClassExampleComponent } from './ng-class-example/ng-class-example.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgSwitchExampleComponent,
-    NgStyleExampleComponent
+    NgStyleExampleComponent,
+    NgClassExampleComponent
   ],
   imports: [
     BrowserModule,
